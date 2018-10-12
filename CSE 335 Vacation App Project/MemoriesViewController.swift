9eec4af9daa0c969d81a7b41d7d873cd73c91e63
Paul <inbox@paulgg.com>
2018-10-12 01:12:42 -0700

    initial boilerplate

---
+++ CSE 335 Vacation App Project/MemoriesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MemoriesViewController.swift
+//  CSE 335 Vacation App Project
+//
+//  Created by Paul Gellai on 10/12/18.
+//  Copyright © 2018 Paul Gellai. All rights reserved.
+//
+
+import UIKit
+
+class MemoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
+        // do nothing
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CSE 335 Vacation App Project/NearMeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NearMeViewController.swift
+//  CSE 335 Vacation App Project
+//
+//  Created by Paul Gellai on 10/12/18.
+//  Copyright © 2018 Paul Gellai. All rights reserved.
+//
+
+import UIKit
+
+class NearMeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
