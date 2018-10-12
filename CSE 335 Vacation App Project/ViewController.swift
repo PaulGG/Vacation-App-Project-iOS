8830e5da252a007e166fee0901f81a19159f711a
Paul <inbox@paulgg.com>
2018-10-12 00:45:08 -0700

    Initial Commit

---
+++ CSE 335 Vacation App Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSE 335 Vacation App Project
+//
+//  Created by Paul Gellai on 10/12/18.
+//  Copyright © 2018 Paul Gellai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CSE 335 Vacation App ProjectTests/CSE_335_Vacation_App_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  CSE_335_Vacation_App_ProjectTests.swift
+//  CSE 335 Vacation App ProjectTests
+//
+//  Created by Paul Gellai on 10/12/18.
+//  Copyright © 2018 Paul Gellai. All rights reserved.
+//
+
+import XCTest
+@testable import CSE_335_Vacation_App_Project
+
+class CSE_335_Vacation_App_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
